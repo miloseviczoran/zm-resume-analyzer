e2ff294f52c1e5d0b3b2775b511dcabe064a6a77
Zoran <mag.milosevic.zoran@gmail.com>
2025-07-31 10:16:20 +0200

    added link in Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,8 +4,8 @@ import { Link } from "react-router";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="/">
-        <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+      <Link to="https://beautiful-portfolio-three.vercel.app">
+        <p className="text-2xl font-bold text-gradient">ZORAN MILOSEVIC</p>
       </Link>
       <Link to="/upload" className="primary-button w-fit">
         Upload Resume
